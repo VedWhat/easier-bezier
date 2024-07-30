2b1e202f7b1af9e9f857913f1d7a6f536ebf606d
ved <ved.chitnis@gmail.com>
2024-07-30 07:21:50 +0530

    improved datastructures for exporting and better redraw

--- improved_index.js
+++ improved_index.js
@@ -6,9 +6,12 @@ document.addEventListener("DOMContentLoaded", () => {
   let tempCurve = [];
   let tempPoints = [];
   let curves = [];
-  let currentCurveOrder = 3;
+  let currentCurveOrder = 9;
   let mousePos;
   let incompleteCurve = false;
+  const d = 0.1;
+
+  let curveCollection = [];
   const factorials = [1, 1, 2, 6, 24, 120, 720, 5040, 40320, 362880, 3628800];
 
   function resizeCanvas() {
@@ -50,29 +53,8 @@ document.addEventListener("DOMContentLoaded", () => {
     redrawCanvas();
   }
 
-  // function lerp(p1, p2, t) {
-  //   let x1 = p1.x + (p2.x - p1.x) * t;
-  //   let y1 = p1.y + (p2.y - p1.y) * t;
-  //   lerpPoints.push({ x: x1, y: y1 });
-  //   return { x: x1, y: y1 };
-  // }
-
-  // let delta = 0.02;
-  // function caclateBezier() {
-  //   p1 = points[0];
-  //   p2 = points[1];
-  //   sp1 = supportPoints[0];
-  //   for (i = 0; i <= 1; i += delta) {
-  //     tp1 = lerp(p1, sp1, i);
-  //     tp2 = lerp(sp1, p2, i);
-  //     fp = lerp(tp1, tp2, i);
-  //     tempCurve.push(fp);
-  //   }
-  // }
-
   function calculateBezierForGeneralCurve(tempCurve, n) {
     // Formula for bezier = Σ(nCk)*(1-t)^(n-i)t^iPi
-    const d = 0.1;
     let bezierCurve = [];
     bezierCurve.push(tempCurve[0]);
 
@@ -89,8 +71,13 @@ document.addEventListener("DOMContentLoaded", () => {
       bezierCurve.push({ x: x, y: y });
     }
     bezierCurve.push(tempCurve[tempCurve.length - 1]);
+    curveCollection.push({
+      curve: bezierCurve,
+      supportPoints: tempCurve.slice(1, -1),
+      startPoint: tempCurve[0],
+      endPoint: tempCurve[tempCurve.length - 1],
+    });
     curves.push(bezierCurve);
-    console.log(tempCurve);
     tempCurve = [];
   }
 
@@ -98,49 +85,54 @@ document.addEventListener("DOMContentLoaded", () => {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     points.forEach((point) => {
       ctx.beginPath();
-      ctx.arc(point.x, point.y, 1.5, 0, 2 * Math.PI);
+      ctx.arc(point.x, point.y, 4, 0, 2 * Math.PI);
       ctx.fill();
     });
 
     if (mousePos && incompleteCurve) {
       ctx.beginPath();
       ctx.fillStyle = "red";
-      ctx.lineDash = [5, 15];
       ctx.arc(mousePos.x, mousePos.y, 5, 0, 2 * Math.PI);
       ctx.fill();
+      ctx.setLineDash([5, 15]);
+
+      for (let i = 0; i < tempPoints.length - 1; i++) {
+        ctx.moveTo(tempPoints[i].x, tempPoints[i].y);
+        ctx.lineTo(tempPoints[i + 1].x, tempPoints[i + 1].y);
+      }
       ctx.moveTo(
         tempPoints[tempPoints.length - 1].x,
         tempPoints[tempPoints.length - 1].y
       );
       ctx.lineTo(mousePos.x, mousePos.y);
       ctx.stroke();
+      ctx.setLineDash([]);
     }
 
-    tempCurve.forEach((curvePoint) => {
+    curveCollection.forEach((c) => {
       ctx.beginPath();
-      ctx.fillStyle = "green";
-      ctx.arc(curvePoint.x, curvePoint.y, 5, 0, 2 * Math.PI);
-      ctx.fill();
-      ctx.beginPath();
-      ctx.arc(curvePoint.x, curvePoint.y, 10, 0, 2 * Math.PI);
+      ctx.setLineDash([]);
+      ctx.moveTo(c.curve[0].x, c.curve[0].y);
+      for (i = 1; i < c.curve.length; i++) {
+        ctx.lineTo(c.curve[i].x, c.curve[i].y);
+      }
       ctx.stroke();
     });
 
-    if (tempCurve.length >= 1) {
+    curveCollection.forEach((c) => {
+      const startPoint = c.startPoint;
+      const endPoint = c.endPoint;
+      const supportPoints = c.supportPoints;
       ctx.beginPath();
-      ctx.moveTo(tempCurve[0].x, tempCurve[0].y);
-      for (i = 1; i < tempCurve.length; i++) {
-        ctx.lineTo(tempCurve[i].x, tempCurve[i].y);
+      ctx.setLineDash([5, 15]);
+      ctx.moveTo(startPoint.x, startPoint.y);
+      for (let i = 0; i < supportPoints.length; i++) {
+        let point = supportPoints[i];
+        ctx.lineTo(point.x, point.y);
+        ctx.moveTo(point.x, point.y);
+        ctx.stroke();
       }
-      ctx.stroke();
-    }
-
-    curves.forEach((c) => {
-      ctx.beginPath();
-      ctx.moveTo(c[0].x, c[0].y);
-      for (i = 1; i < c.length; i++) {
-        ctx.lineTo(c[i].x, c[i].y);
-      }
+      ctx.lineTo(endPoint.x, endPoint.y);
       ctx.stroke();
     });
   }
